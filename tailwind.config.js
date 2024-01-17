41c172ccbf1f3bd65e9a4d43261996660c58cdde
ashik-08 <ashikurrahman.8october@gmail.com>
2024-01-18 00:58:00 +0600

    [feat](setup): initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      play: ["Playfair Display", "serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        special: "#0073FF",
+        head: "#00D4FF",
+        "sub-head": "#333333",
+        details: "#888888",
+      },
+    },
+  },
+  plugins: [],
+};
