6106372ed71f19f675d87fce96d1033c6080b337
JoujonikiAsa2 <joujonikiasaroy@gmail.com>
2024-01-19 00:09:58 +0600

    Added signup page ui

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
+import Login from "../pages/Login/Login";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
   },
+  {
+    path: '/login1',
+    element: <Login></Login>
+  }
 ]);
