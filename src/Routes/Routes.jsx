b750c73fee79d9aacf3fa9dc3cd0e066acd23dbd
shihab01118 <shahed.shihab.0@gmail.com>
2024-01-18 20:41:12 +0600

    [feat](signup): signup page design completed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
   },
+  {
+    path: "signup",
+    element: <SignUp />
+  }
 ]);
