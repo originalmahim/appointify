330ebcf56c634a6327dc9f896dad7025568e8f44
shihab01118 <shahed.shihab.0@gmail.com>
2024-01-19 18:09:28 +0600

    [feature](signup): authentication added for signup and google login

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,10 +1,46 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const SocialLogin = () => {
+  const { signInWithGoogle } = useAuth();
+  const navigate = useNavigate();
+
+  // login with google
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithGoogle();
+      console.log(result?.user);
+      
+      if (result?.user) {
+        toast.success("Login Successful!", {
+          style: {
+            borderRadius: "8px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Login Failed!", {
+        style: {
+          borderRadius: "8px",
+          background: "#333",
+          color: "#fff",
+        },
+      });
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2 font-inter text-sm text-gray-900">
-      <button className="w-full border-2 h-[44px] font-semibold bg-gradient-to-r from-special to-head p-[1px] rounded-lg relative transform active:scale-95 transition">
+      <button
+        onClick={handleGoogleSignIn}
+        className="w-full border-2 h-[44px] font-semibold bg-gradient-to-r from-special to-head p-[1px] rounded-lg relative transform active:scale-95 transition"
+      >
         <div className="bg-white h-full w-full flex items-center justify-center rounded-lg hover:bg-[#E4EDFF] transition">
           <span className="absolute left-3">
             <FcGoogle size={20} />
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./Routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Provider/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
+        <Toaster />
         <RouterProvider router={routes} />
       </HelmetProvider>
     </AuthProvider>
