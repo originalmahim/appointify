9c0ea89005f68816fe5d4910b6430e99c64709f7
JoujonikiAsa2 <joujonikiasaroy@gmail.com>
2024-01-23 16:58:59 +0600

    Added blog page, blog card, blog details

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import MainLayout from "../layout/MainLayout";
 import HomePage from "../pages/HomePage/HomePage";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Login from "../pages/Login/Login";
+import BlogPage from "../pages/Blog/BlogPage";
+import BlogDetails from "../components/Blog/BlogDetails";
 
 export const routes = createBrowserRouter([
   {
@@ -15,6 +17,15 @@ export const routes = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
+      {
+        path: '/blogs',
+        element: <BlogPage></BlogPage>
+      },
+      {
+        path: '/blog/:id',
+        element: <BlogDetails></BlogDetails>,
+        loader: () => fetch('blogs.json')
+      }
     ],
   },
   {
