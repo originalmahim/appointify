78d8c386db6c892db10851d01d35a355dfee8751
shihab01118 <shahed.shihab.0@gmail.com>
2024-01-31 19:36:11 +0600

    [feat](inbox): Pagination added to the inbox page

--- src/pages/Dashboard/Inbox/Inbox.jsx
+++ src/pages/Dashboard/Inbox/Inbox.jsx
@@ -1,12 +1,46 @@
 import { Helmet } from "react-helmet-async";
 import TableRow from "../../../components/Inbox/TableRow";
-import useGetMessages from "../../../hooks/useGetMessages";
-import { Card, Typography } from "@material-tailwind/react";
+import { Typography, IconButton } from "@material-tailwind/react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const TABLE_HEAD = ["Name", "Email", "Message", "Actions"];
 
 const Inbox = () => {
-  const { messages, refetch } = useGetMessages();
+  const [active, setActive] = useState(1);
+  const axiosSecure = useAxiosSecure();
+  
+  const { data: count = 0 } = useQuery({
+    queryKey: ["messageCount", axiosSecure],
+    queryFn: async () => {
+      const { data } = await axiosSecure("/messages/count");
+      return data?.count;
+    },
+  });
+
+  const pageCount = Math.ceil(count / 10);
+
+  const { data: messages, refetch } = useQuery({
+    queryKey: ["messages", active, axiosSecure],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/messages?active=${active}`);
+      return data;
+    },
+  });
+
+  const next = () => {
+    if (active === pageCount) return;
+
+    setActive(active + 1);
+  };
+
+  const prev = () => {
+    if (active === 1) return;
+
+    setActive(active - 1);
+  };
 
   return (
     <>
@@ -14,7 +48,6 @@ const Inbox = () => {
         <title>Dashboard | Inbox</title>
       </Helmet>
       <section className="overflow-x-auto">
-        <Card className="w-full h-full">
           <table className="w-full min-w-max table-auto text-left">
             <thead>
               <tr>
@@ -49,7 +82,28 @@ const Inbox = () => {
               })}
             </tbody>
           </table>
-        </Card>
+          <div className="flex items-center gap-8 absolute bottom-10 left-1/2 transform -translate-x-1/2 md:-translate-x-0">
+            <IconButton
+              size="sm"
+              variant="outlined"
+              onClick={prev}
+              disabled={active === 1}
+            >
+              <FaArrowLeft stroke="2" className="h-4 w-4" />
+            </IconButton>
+            <Typography color="gray" className="font-normal min-w-[84px]">
+              Page <strong className="text-gray-900">{active}</strong> of{" "}
+              <strong className="text-gray-900">{pageCount}</strong>
+            </Typography>
+            <IconButton
+              size="sm"
+              variant="outlined"
+              onClick={next}
+              disabled={active === pageCount}
+            >
+              <FaArrowRight stroke="2" className="h-4 w-4" />
+            </IconButton>
+          </div>
       </section>
     </>
   );
