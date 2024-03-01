91d8f9a4e5b243ca8b0ab1bf4d6434f6998543cf
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2024-03-01 15:03:06 +0600

    feat(user available slots):Displayed the user avalable slots in dynamic page

--- src/pages/DynamicMeetingPage/Slots.jsx
+++ src/pages/DynamicMeetingPage/Slots.jsx
@@ -7,62 +7,17 @@ const Slots = ({ slotDay }) => {
   const [userSlots, setUserSlots] = useState([]);
 
   useEffect(() => {
-    axios.get("/users/shakilahmmed8882@gmail.com").then((res) => {
+    axios.get("/users/availability/shakilahmmed8882@gmail.com").then((res) => {
       if (res?.data?.length > 0) {
         setUserSlots(res.data);
       }
     });
   }, [axios]);
 
-  const daysWithSlots = [
-    {
-      day: "Wednesday",
-      slots: [
-        { start_time: "09:00am", end_time: "10:00am" },
-        { start_time: "01:30pm", end_time: "02:30pm" },
-        { start_time: "04:00pm", end_time: "05:00pm" },
-      ],
-    },
-    {
-      day: "Friday",
-      slots: [
-        { start_time: "10:30am", end_time: "11:30am" },
-        { start_time: "02:00pm", end_time: "03:00pm" },
-        { start_time: "05:30pm", end_time: "06:30pm" },
-      ],
-    },
-    {
-      day: "Sunday",
-      slots: [
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-        { start_time: "08:00am", end_time: "09:00am" },
-        { start_time: "12:30pm", end_time: "01:30pm" },
-        { start_time: "03:00pm", end_time: "04:00pm" },
-      ],
-    },
-  ];
-
   return (
     <div>
       {/* Show only the slot based on selected day on calendar */}
-      {daysWithSlots
+      {userSlots
         .filter((day) => day.day === slotDay)
         .map((day) => (
           <div key={day.day} className="">
