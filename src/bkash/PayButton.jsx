75c41490b1802da584fbf8379f36907e7599fceb
sadmanryanriad <sadmanryanriad@gmail.com>
2024-03-05 17:23:34 +0600

    integrated pay button with bakcend

--- src/bkash/PayButton.jsx
+++ src/bkash/PayButton.jsx
@@ -1,12 +1,29 @@
-const PayButton = ({children}) => {
-    const payButton = () =>{
-        console.log("pay button clicked");
+import axios from "axios";
+
+const PayButton = ({ children }) => {
+  const pay = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/bkash/payment/create",
+        { amount: 50, orderId: 1 },
+        { withCredentials: true }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error.response.data);
     }
-    return (
-        <div>
-            <button  className="w-full h-12 text-white font-semibold bg-gradient-blue rounded-lg hover:bg-gradient-to-r hover:from-special hover:to-head active:scale-95" onClick={payButton}>{children}</button>
-        </div>
-    );
+  };
+
+  return (
+    <div>
+      <button
+        className="w-full h-12 text-white font-semibold bg-gradient-blue rounded-lg hover:bg-gradient-to-r hover:from-special hover:to-head active:scale-95"
+        onClick={pay}
+      >
+        {children}
+      </button>
+    </div>
+  );
 };
 
 export default PayButton;
